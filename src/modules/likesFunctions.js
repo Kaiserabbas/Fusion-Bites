c83949a557f5bd9673ed743f096b8308ef64dfc6
Qaisar <kayser.abbas@gmail.com>
2023-08-09 08:12:33 +0500

    Add functions for like button interactions

---
+++ src/modules/likesFunctions.js
@@ -0,0 +1,44 @@
+import { apiUrl } from './api.js';
+
+async function getLikes() {
+  try {
+    const response = await fetch(`${apiUrl}/likes`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch likes with status: ${response.status}`);
+    }
+
+    const likesData = await response.json();
+    return likesData;
+  } catch (error) {
+    console.error('Error fetching likes:', error);
+    return [];
+  }
+}
+
+async function saveLikes(itemId) {
+  try {
+    const response = await fetch(`${apiUrl}/likes`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ item_id: itemId }),
+    });
+
+    if (!response.ok) {
+      throw new Error(`Failed to save like with status: ${response.status}`);
+    }
+
+    console.log('Like saved successfully');
+  } catch (error) {
+    console.error('Error saving like:', error);
+  }
+}
+
+export { getLikes, saveLikes };
