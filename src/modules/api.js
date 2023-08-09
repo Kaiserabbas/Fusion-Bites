36291f79df2a29c61e6a92929df131a2e5d90c59
Qaisar <kayser.abbas@gmail.com>
2023-08-09 08:11:03 +0500

    Add API for likes interactions

--- src/modules/api.js
+++ src/modules/api.js
@@ -1,3 +1,4 @@
 const FOOD_BASE_URL = 'https://themealdb.com/api/json/v1/1/filter.php?a=Indian';
-
-export default FOOD_BASE_URL;
+const appId = 'K3sxVOiMOhEMJTSuvB1D';
+const apiUrl = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${appId}`;
+export { FOOD_BASE_URL, apiUrl };
