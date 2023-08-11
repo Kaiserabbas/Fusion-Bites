1e12ab81351c22693486d160c931a4f6b69a224a
misikira <misikirteka@gmail.com>
2023-08-11 10:51:03 +0300

    add the comment feature

---
+++ src/modules/getComments.js
@@ -0,0 +1,21 @@
+const sendComment = async (id, username, comment) => {
+    const request = await fetch(`${apiUrl}/comments/`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        item_id: id,
+        username: username,
+        comment: comment,
+      }),
+    });
+  };
+  
+  const fetchComments = async (id) => {
+    const request = await fetch(`${apiUrl}/comments?item_id=${id}`);
+    const response = await request.json();
+    return response;
+  };
+  
+  export { sendComment, fetchComments };
